cfd027b78d66343a34a84018854fd51e9473d19d
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-16 10:06:08 +0900

    fix: import services for credential store

--- src/DKImageAIEditor/MainWindow.RequestFeatures.cs
+++ src/DKImageAIEditor/MainWindow.RequestFeatures.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using DKImageAIEditor.Models;
+using DKImageAIEditor.Services;
 
 namespace DKImageAIEditor;
 
